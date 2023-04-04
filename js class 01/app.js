2ccb607b1631f8651ac8431d50da894c7e2860d7
safiabano <safiabano736@gmail.com>
2023-04-04 11:01:20 +0500

    JS first assignment

---
+++ js class 01/app.js
@@ -0,0 +1,38 @@
+// let myName="safia"; // var // variable //let
+// alert(myName);
+//variables rules
+//1.legal or illigal name
+//legal
+//lastName//camelcase
+//allowed special characters
+//_ $
+//illigal
+//last name
+//#/
+//reserved words
+//data types
+//string
+//all types special character
+////a-z in ""
+//any letter word sentences are string in javascrript
+//numbers -7 45 1 4.77
+//boolean 
+//let isTodayMonday=true
+
+
+//================= assignmentwork==============
+
+//let myData ="Eror! please enter a valid password.";
+//alert(myData);
+
+//let questionTwo="Welcome To JS Land...\n Happy Coding!"
+//alert(questionTwo)
+
+//let thirdQuetion="Welcome To JS Land...";
+//alert(thirdQuetion);
+
+//let forthQuestion="Happy Coding!\n Prevent this page from creating additional Dialogs.";
+//alert(forthQuestion);
+
+let fifthQuestion="Hello... I can run JS through my web browsre's console ";
+alert(fifthQuestion)
